8cb0aa50fba7b903d0b74f4df7e4c216d43f3cfc
DieNice <DailyPlanning@bk.ru>
2020-08-01 20:08:40 +1000

    [#3] added comment for target name

--- main_backup.py
+++ main_backup.py
@@ -6,6 +6,13 @@ target_dir = "/home/pda/Documents/"
 today = target_dir + os.sep + time.strftime("%Y%m%d")
 now = time.strftime("%H%M%S")
 
+comment = input("Please, input commit:")
+if len(comment) == 0:
+    target_name = today + os.sep + now + '.zip'
+else:
+    target_name = today + os.sep + now + '_' \
+                  + comment.replace(' ', '_') + '.zip'
+
 if not os.path.exists(today):
     os.mkdir(today)
     print("directory created successfully", today)
