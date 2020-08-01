68292961be5185f65ceb74156dae767a92fc3ed6
DieNice <DailyPlanning@bk.ru>
2020-08-01 19:49:51 +1000

    [#2] added creation sub directory with name of now date for zip file with now datetime

--- main_backup.py
+++ main_backup.py
@@ -3,9 +3,15 @@ import time
 
 source = ["/home/pda/Documents/Self-development"]
 target_dir = "/home/pda/Documents/"
-target_name = target_dir + os.sep + time.strftime('%Y%m%d%H%M%S') + '.zip'
+today = target_dir + os.sep + time.strftime("%Y%m%d")
+now = time.strftime("%H%M%S")
+
+if not os.path.exists(today):
+    os.mkdir(today)
+    print("directory created successfully", today)
+target_name = today + os.sep + now + '.zip'
 zip_command = "zip -qr {0} {1}".format(target_name, ' '.join(source))
 if os.system(zip_command) == 0:
-    print("Copy is sucсsessfully created", target_name)
+    print("Copy is sucсessfully created", target_name)
 else:
     print("No copy created")
